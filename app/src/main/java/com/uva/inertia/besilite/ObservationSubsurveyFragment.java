6e55d8d8356de29d6b66d33db6379d803dbec76f
Ben Ghaem <ben.ghaem@gmail.com>
2016-02-25 01:04:48 -0500

    Fix radio button handler

--- app/src/main/java/com/uva/inertia/besilite/ObservationSubsurveyFragment.java
+++ app/src/main/java/com/uva/inertia/besilite/ObservationSubsurveyFragment.java
@@ -1,10 +1,6 @@
 package com.uva.inertia.besilite;
 
-import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -58,33 +54,33 @@ public class ObservationSubsurveyFragment extends android.support.v4.app.Fragmen
         pwdObs = ar.pwdObs;
 
         Kicking = (CheckBox) rootView.findViewById(R.id.checkKicking);
-        Kicking.setOnClickListener(CustomClickHandlers.updateMapOnClick(pwdObs, "Kicking"));
+        Kicking.setOnClickListener(CustomClickHandlers.updateMapOnRadio(pwdObs, "Kicking"));
         Hitting = (CheckBox) rootView.findViewById(R.id.checkHitting);
-        Hitting.setOnClickListener(CustomClickHandlers.updateMapOnClick(pwdObs, "Hitting"));
+        Hitting.setOnClickListener(CustomClickHandlers.updateMapOnRadio(pwdObs, "Hitting"));
         Pacing = (CheckBox) rootView.findViewById(R.id.checkPacing);
-        Pacing.setOnClickListener(CustomClickHandlers.updateMapOnClick(pwdObs, "Pacing"));
+        Pacing.setOnClickListener(CustomClickHandlers.updateMapOnRadio(pwdObs, "Pacing"));
         Wandering = (CheckBox) rootView.findViewById(R.id.checkWandering);
-        Wandering.setOnClickListener(CustomClickHandlers.updateMapOnClick(pwdObs, "Wandering"));
+        Wandering.setOnClickListener(CustomClickHandlers.updateMapOnRadio(pwdObs, "Wandering"));
         Disrobing = (CheckBox) rootView.findViewById(R.id.checkDisrobing);
-        Disrobing.setOnClickListener(CustomClickHandlers.updateMapOnClick(pwdObs, "Disrobing"));
+        Disrobing.setOnClickListener(CustomClickHandlers.updateMapOnRadio(pwdObs, "Disrobing"));
         Scratching = (CheckBox) rootView.findViewById(R.id.checkScratching);
-        Scratching.setOnClickListener(CustomClickHandlers.updateMapOnClick(pwdObs, "Scratching"));
+        Scratching.setOnClickListener(CustomClickHandlers.updateMapOnRadio(pwdObs, "Scratching"));
         ScreamingYelling = (CheckBox) rootView.findViewById(R.id.checkScreamingYelling);
-        ScreamingYelling.setOnClickListener(CustomClickHandlers.updateMapOnClick(pwdObs, "ScreamingYelling"));
+        ScreamingYelling.setOnClickListener(CustomClickHandlers.updateMapOnRadio(pwdObs, "ScreamingYelling"));
         StrangeNoises = (CheckBox) rootView.findViewById(R.id.checkStrangeNoises);
-        StrangeNoises.setOnClickListener(CustomClickHandlers.updateMapOnClick(pwdObs, "StrangeNoises"));
+        StrangeNoises.setOnClickListener(CustomClickHandlers.updateMapOnRadio(pwdObs, "StrangeNoises"));
         Repetitive = (CheckBox) rootView.findViewById(R.id.checkRepetitive);
-        Repetitive.setOnClickListener(CustomClickHandlers.updateMapOnClick(pwdObs, "Repetitive"));
+        Repetitive.setOnClickListener(CustomClickHandlers.updateMapOnRadio(pwdObs, "Repetitive"));
         Biting = (CheckBox) rootView.findViewById(R.id.checkBiting);
-        Biting.setOnClickListener(CustomClickHandlers.updateMapOnClick(pwdObs, "Biting"));
+        Biting.setOnClickListener(CustomClickHandlers.updateMapOnRadio(pwdObs, "Biting"));
         Spitting = (CheckBox) rootView.findViewById(R.id.checkSpitting);
-        Spitting.setOnClickListener(CustomClickHandlers.updateMapOnClick(pwdObs, "Spitting"));
+        Spitting.setOnClickListener(CustomClickHandlers.updateMapOnRadio(pwdObs, "Spitting"));
         EatingNonFood = (CheckBox) rootView.findViewById(R.id.checkEatingNonFood);
-        EatingNonFood.setOnClickListener(CustomClickHandlers.updateMapOnClick(pwdObs, "EatingNonFood"));
+        EatingNonFood.setOnClickListener(CustomClickHandlers.updateMapOnRadio(pwdObs, "EatingNonFood"));
         BreakingThings = (CheckBox) rootView.findViewById(R.id.checkBreakingThings);
-        BreakingThings.setOnClickListener(CustomClickHandlers.updateMapOnClick(pwdObs, "BreakingThings"));
+        BreakingThings.setOnClickListener(CustomClickHandlers.updateMapOnRadio(pwdObs, "BreakingThings"));
         UnableToFindCommonPlaces = (CheckBox) rootView.findViewById(R.id.checkUnableToFindCommonPlaces);
-        UnableToFindCommonPlaces.setOnClickListener(CustomClickHandlers.updateMapOnClick(pwdObs, "UnableToFindCommonPlaces"));
+        UnableToFindCommonPlaces.setOnClickListener(CustomClickHandlers.updateMapOnRadio(pwdObs, "UnableToFindCommonPlaces"));
 
 
         return rootView;
