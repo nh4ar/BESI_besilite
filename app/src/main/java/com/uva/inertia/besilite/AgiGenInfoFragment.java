c80f812837182a70369d1421e79e425ca9b50926
ng4mf <ng4mf@virginia.edu>
2016-03-11 01:46:09 -0500

    Not sure I changed anything, but I'm told I did

--- app/src/main/java/com/uva/inertia/besilite/AgiGenInfoFragment.java
+++ app/src/main/java/com/uva/inertia/besilite/AgiGenInfoFragment.java
@@ -206,6 +206,47 @@ public class AgiGenInfoFragment extends Fragment implements passBackInterface{
         updateMapDatetime();
     }
 
+
+
+
+
+
+
+
+
+
+
+    private class DurationTimePickDialog extends TimePickerDialog
+    {
+        final OnTimeSetListener mCallback;
+        TimePicker mTimePicker;
+        final int increment;
+
+        public DurationTimePickDialog(Context context, OnTimeSetListener callBack, int hourOfDay, int minute, boolean is24HourView, int increment)
+        {
+            super(context, callBack, hourOfDay, minute/increment, is24HourView);
+            this.mCallback = callBack;
+            this.increment = increment;
+        }
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            if (mCallback != null && mTimePicker!=null) {
+                mTimePicker.clearFocus();
+                mCallback.onTimeSet(mTimePicker, mTimePicker.getCurrentHour(),
+                        mTimePicker.getCurrentMinute()*increment);
+            }
+        }
+
+        @Override
+        protected void onStop()
+        {
+            // override and do nothing
+        }
+
+
+    }
+
     public static class TimePickerFragment extends DialogFragment
             implements TimePickerDialog.OnTimeSetListener {
 
@@ -236,6 +277,20 @@ public class AgiGenInfoFragment extends Fragment implements passBackInterface{
         newFragment.setTargetFragment(this, TIMEPICKER_FRAGMENT);
         newFragment.show((getActivity()).getSupportFragmentManager().beginTransaction(), "agiTimePicker");
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     public static class DatePickerFragment extends DialogFragment
@@ -283,63 +338,6 @@ public class AgiGenInfoFragment extends Fragment implements passBackInterface{
         }
     }
 
-
-
-    private class DurationTimePickDialog extends TimePickerDialog
-    {
-        final OnTimeSetListener mCallback;
-        TimePicker mTimePicker;
-        final int increment;
-
-        public DurationTimePickDialog(Context context, OnTimeSetListener callBack, int hourOfDay, int minute, boolean is24HourView, int increment)
-        {
-            super(context, callBack, hourOfDay, minute/increment, is24HourView);
-            this.mCallback = callBack;
-            this.increment = increment;
-        }
-
-        @Override
-        public void onClick(DialogInterface dialog, int which) {
-            if (mCallback != null && mTimePicker!=null) {
-                mTimePicker.clearFocus();
-                mCallback.onTimeSet(mTimePicker, mTimePicker.getCurrentHour(),
-                        mTimePicker.getCurrentMinute()*increment);
-            }
-        }
-
-        @Override
-        protected void onStop()
-        {
-            // override and do nothing
-        }
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState)
-        {
-            super.onCreate(savedInstanceState);
-            try
-            {
-                Class<?> rClass = Class.forName("com.android.internal.R$id");
-                Field timePicker = rClass.getField("timePicker");
-                this.mTimePicker = (TimePicker)findViewById(timePicker.getInt(null));
-                Field m = rClass.getField("minute");
-
-                NumberPicker mMinuteSpinner = (NumberPicker)mTimePicker.findViewById(m.getInt(null));
-                mMinuteSpinner.setMinValue(0);
-                mMinuteSpinner.setMaxValue((60/increment)-1);
-                List<String> displayedValues = new ArrayList<String>();
-                for(int i=0;i<60;i+=increment)
-                {
-                    displayedValues.add(String.format("%02d", i));
-                }
-                mMinuteSpinner.setDisplayedValues(displayedValues.toArray(new String[0]));
-            }
-            catch (Exception e)
-            {
-                e.printStackTrace();
-            }
-        }
-    }
 
 }
 
